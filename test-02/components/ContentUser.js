c7ee09dc48fa3e78198bd517b3a76d9b1408627e
Fernando Casaliba Perlangeli <fernando.casaliba@gmail.com>
2017-04-03 12:20:29 -0300

    fetch a  la API de Github

--- test-02/components/ContentUser.js
+++ test-02/components/ContentUser.js
@@ -11,64 +11,97 @@ export default class ContentUser extends React.Component {
   constructor() {
     super();   
     this.state = {
-      user            : 'facasalibaper',
+      user             : 'fcasalibaper',
+      bio              : 'bio',
       name             : 'Fer',
       repositorios     : '3',
       seguidores       : '3',
       siguiendo        : '6',
+      repo_url         : 'dsadasd',
+      seguidores_url   : 'hththth',
+      siguiendo_url    : 'dsadas',
       url              : '#',
+      email            : 'p@gmail.com',
       imageProfile     : 'https://avatars1.githubusercontent.com/u/6304138?v=3'    
     };
   }
 
+  // Call AJAX´s
+  componentWillMount() {  
+    fetch(API+'fcasalibaper')  
+      .then((response) => {
+          return response.json()
+      })
+      .then((data) => {
+          console.log(data)
+          this.setState({
+            user            : data.login,
+            bio             : data.bio,
+            name            : data.name,
+            repositorios    : data.public_repos,
+            seguidores      : data.followers,
+            siguiendo       : data.following,
+            repo_url        : data.repos_url,
+            seguidores_url  : data.followers_url,
+            siguiendo_url   : data.following_url,
+            url             : data.url,
+            email           : data.email,
+            imageProfile    : data.avatar_url
+          })
+
+      })
+  }
+
   render() { 
-   return (       
-    <div className="app__profile">  
-      
-      <Searchbox />
-      <div className="app__profile__content">
-        <hgroup className="app__title">
-          <h3>Github profile</h3>
-        </hgroup>
+    return (       
+      <div className="app__profile">      
+        <Searchbox />
+        <div className="app__profile__content">
+          <hgroup className="app__title">
+            <h3>Github profile</h3>
+          </hgroup>
 
-        <figure className="app__images">
-          <aside className="app__bg"></aside>            
-          <picture className="app__profilePic">
-            <img src={this.state.imageProfile} />
-          </picture>
-        </figure>
+          <figure className="app__images">
+            <aside className="app__bg"></aside>            
+            <picture className="app__profilePic">
+              <img src={this.state.imageProfile} />
+            </picture>
+          </figure>
 
-        <header className="app__name">
-          <h1>
-            {this.state.name}
-            <small>{this.state.shortdescription}</small>
-          </h1>
-          <a className="btn" href={this.state.url}><span>Profile</span></a>
-        </header>
+          <header className="app__name">
+            <h1>
+              {this.state.name}
+              <small>
+                {this.state.bio}
+                <a href={'mailto:'+this.state.email} target="_self">{this.state.email}</a>
+              </small>
+            </h1>
+            <a className="btn" href={this.state.url}><span>Profile</span></a>
+          </header>
 
-        <ul className="app__data">
-          <li className="app__data__features">
-            <a href={this.state.url+'?tab=followers'} target="_blank">
-              {this.state.seguidores}
-              <span>Seguidores</span>   
-            </a>  
-          </li>
-          <li className="app__data__features">
-            <a href={this.state.url+'?tab=repositories'} target="_blank">
-              {this.state.repositorios}
-              <span>Repositorios</span>
-            </a>  
+          <ul className="app__data">
+            <li className="app__data__features">
+              <a href={this.state.url+'?tab=followers'} target="_blank">
+                {this.state.seguidores}
+                <span>Seguidores</span>   
+              </a>  
+            </li>
+            <li className="app__data__features">
+              <a href={this.state.url+'?tab=repositories'} target="_blank">
+                {this.state.repositorios}
+                <span>Repositorios</span>
+              </a>  
 
-          </li>
-          <li className="app__data__features">
-            <a href={this.state.url+'?tab=following'} target="_blank">
-              {this.state.siguiendo}
-              <span>Siguiendo</span>    
-            </a>  
-          </li>
-        </ul>
+            </li>
+            <li className="app__data__features">
+              <a href={this.state.url+'?tab=following'} target="_blank">
+                {this.state.siguiendo}
+                <span>Siguiendo</span>    
+              </a>  
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
     )
   }
 }
--- test-02/components/Searchbox.js
+++ test-02/components/Searchbox.js
@@ -2,10 +2,23 @@
 import React from 'react';
 
 export default class Searchbox extends React.Component {
+	
+	constructor() {
+		super();
+		this.state = {			
+			typed : ''			
+		}	
+	}
+
+	onChange(event) {
+		this.setState({typed : event.target.value})		
+	}
+
 	render() { 
    	return (
 			<form>
-			  <input className="app__search" placeholder="search profile..."/>
+				<div>{this.state.typed}</div>
+			  <input className="app__search" placeholder="search profile..." value={this.state.user} onChange={this.onChange.bind(this)}/>
 			</form>
 		)
  	}
